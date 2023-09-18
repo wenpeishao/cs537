b6ddbd7f4799916526dcf189fac57a1f68aefdd7
Wenpei Shao <clevertonyshao@gmail.com>
2023-09-17 23:52:17 -0500

    update

---
+++ p1/wgroff.c
@@ -0,0 +1,139 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <assert.h>
+#include <ctype.h>
+
+char *format_line(char *line)
+{
+    static char fl[1024];      // Make it static so it persists after function returns
+    memset(fl, 0, sizeof(fl)); // Clear the array
+
+    sprintf(fl, "       ");         // Indent 7 spaces
+    char *fl_ptr = fl + strlen(fl); // Move pointer to the end of indentation
+
+    while (*line != '\0')
+    {
+        if (strncmp(line, "/fB", 3) == 0)
+        {
+            strcat(fl_ptr, "\033[1m");
+            line += 3;
+        }
+        else if (strncmp(line, "/fI", 3) == 0)
+        {
+            strcat(fl_ptr, "\033[3m");
+            line += 3;
+        }
+        else if (strncmp(line, "/fU", 3) == 0)
+        {
+            strcat(fl_ptr, "\033[4m");
+            line += 3;
+        }
+        else if (strncmp(line, "/fP", 3) == 0)
+        {
+            strcat(fl_ptr, "\033[0m");
+            line += 3;
+        }
+        else if (strncmp(line, "//", 2) == 0)
+        {
+            strcat(fl_ptr, "/");
+            line += 2;
+        }
+        else
+        {
+            fl_ptr[0] = *line; // Append just the current character
+            fl_ptr[1] = '\0';  // Null-terminate
+            fl_ptr++;          // Move the pointer for next character
+            line++;            // Go to the next character
+        }
+    }
+
+    strcat(fl_ptr, "\n"); // Append newline at the end
+
+    return fl;
+}
+
+int wgroff(const char *input_file)
+{
+    FILE *ifp = fopen(input_file, "r");
+    if (ifp == NULL)
+    {
+        printf("File doesn't exist\n");
+        exit(0);
+    }
+    char line[512];
+    fgets(line, 100, ifp);
+    // check first line get the header
+    char *header[4];
+    int lineno = 1;
+    // header 0 is ".TH"| header 1 is command| header 2 is section| header 3 is date TODO check Bad Header
+    header[0] = strtok(line, " ");
+    if (strstr(header[0], ".TH") == NULL)
+    {
+        printf("Improper formatting on line %i\n", lineno);
+        exit(0);
+    }
+    for (int i = 1; i < 4; i++)
+    {
+        header[i] = strtok(NULL, " ");
+    }
+    char path[518];
+    sprintf(path, "./man_pages/man%s/%s", header[2], header[1]);
+    FILE *nfp = fopen(path, "w+");
+    // write the first line
+    char firstLine[100];
+    char title[80];
+    sprintf(title, "%s(%s)", header[1], header[2]);
+    strcat(firstLine, title);
+    while (strlen(firstLine) <= (80 - strlen(title)))
+    {
+        strcat(firstLine, " ");
+    }
+    strcat(firstLine, title);
+    sprintf(firstLine, "%s\n", firstLine);
+    fputs(firstLine, nfp);
+    while (fgets(line, sizeof(line), ifp) != NULL)
+    {
+        lineno++;
+        if (strstr(line, ".SH"))
+        {
+            char *sh;
+            char subheader[518];
+            sh = strchr(line, ' ');
+            while (*sh) // sh字符串转换大小写
+            {
+
+                sh++;
+            }
+            sprintf(subheader, "%s\n\n", sh);
+            printf("%s", subheader);
+            fputs(sh, nfp);
+            fclose(nfp);
+        }
+        else
+        {
+            char *line = format_line(line);
+        }
+    }
+    while (strlen(line) <= (80 - strlen(header[3])))
+    {
+        strcat(line, " ");
+    }
+    strcat(line, header[3]);
+    fclose(nfp);
+    fclose(ifp);
+    return 0;
+}
+
+int main(int argc, char *argv[])
+{
+    if (argc != 2)
+    {
+        printf("Improper number of arguments\nUsage: ./wgroff <file>\n");
+        return 0;
+    }
+    char path[100];
+    sprintf(path, "./input_files/%s", argv[1]);
+    wgroff(path);
+    return 0;
+}
