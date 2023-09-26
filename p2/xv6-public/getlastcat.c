a60738aa4c78a13d0a01f6f9556afcfe2126a359
wenpeishao <wenpeishao@gmail.com>
2023-09-25 23:25:35 -0500

    update

--- p2/xv6-public/getlastcat.c
+++ p2/xv6-public/getlastcat.c
@@ -5,7 +5,14 @@
 int main(void)
 {
     char buf[512];
-    getlastcat(buf);
-    printf(1, "XV6_TEST_OUTPUT Last catted filename: %s\n", buf);
+    if (getlastcat(buf) < 0)
+    {
+        printf(1, "Failed to get the last catted filename\n");
+    }
+    else
+    {
+        printf(1, "XV6_TEST_OUTPUT Last catted filename: %s\n", buf);
+    }
+
     exit();
 }
