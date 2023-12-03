d785fe483ad2031c34645e72bf01e0b317f59b40
wenpeishao <wenpeishao@gmail.com>
2023-12-03 00:54:44 -0600

    add tester file

--- p7/mount.wfs.c
+++ p7/mount.wfs.c
@@ -7,3 +7,29 @@
  * You need to pass [FUSE options] along with the mount_point to fuse_main as argv.
  * You may assume -s is always passed to mount.wfs as a FUSE option to disable multi-threading.
  */
+#define FUSE_USE_VERSION 30
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+
+static int my_getattr(const char *path, struct stat *stbuf)
+{
+    // Implementation of getattr function to retrieve file attributes
+    // Fill stbuf structure with the attributes of the file/directory indicated by path
+    // ...
+
+    return 0; // Return 0 on success
+}
+
+static struct fuse_operations my_operations = {
+    .getattr = my_getattr,
+    // Add other functions (read, write, mkdir, etc.) here as needed
+};
+
+int main(int argc, char *argv[])
+{
+    // Initialize FUSE with specified operations
+    // Filter argc and argv here and then pass it to fuse_main
+    return fuse_main(argc, argv, &my_operations, NULL);
+}
